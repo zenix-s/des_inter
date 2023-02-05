8a5c3bf49019da6147f3d7082b8e1d0e1045ec4a
shadowborne <sergiofs19@gmail.com>
2023-02-05 13:18:48 +0100

    Update libreria.sql

--- Proyecto/libreria.sql
+++ Proyecto/libreria.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 05-02-2023 a las 11:46:07
+-- Tiempo de generación: 05-02-2023 a las 13:18:10
 -- Versión del servidor: 10.4.27-MariaDB
 -- Versión de PHP: 8.2.0
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `lib`
+-- Base de datos: `libreria`
 --
 
 -- --------------------------------------------------------
